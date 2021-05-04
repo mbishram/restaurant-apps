f59ee77dfdffbf1143e10aedfaa6d2fd61123797
Muh Bishram Yashir Alfarizi A <m.bishram.yashir@gmail.com>
2021-05-04 21:59:55 +0800

    installing prettier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
 
-console.log('Hello Coders! :)');
+console.log("Hello Coders! :)");
