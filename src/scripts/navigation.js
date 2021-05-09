694d8241db05aadba8b5c23a9dfbd26466cd35fd
Muh Bishram Yashir Alfarizi A <m.bishram.yashir@gmail.com>
2021-05-09 11:40:26 +0800

    making navigation on tablet more responsive

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,9 +1,11 @@
 $(() => {
-	const navButton = $("#nav-button");
 	const nav = $("nav");
+	const navButton = $("#nav-button");
 	const navButtonIcon = $("#nav-button i");
+	const body = $("body");
 
-	navButton.click(() => {
+	// Handler for when navigation button is clicked
+	const handleNav = (event) => {
 		// Setting up button
 		navButtonIcon.toggleClass("bi-x");
 		navButtonIcon.toggleClass("bi-list");
@@ -11,19 +13,52 @@ $(() => {
 
 		// Handle the nav
 		nav.toggleClass("open");
+
+		// Stop it from immediately closing nav when nav button is clicked
+		event.stopPropagation();
+	};
+
+	// Function to close nav
+	const closeNav = () => {
+		if (nav.hasClass("open")) {
+			// Setting up button
+			navButtonIcon.removeClass("bi-x");
+			navButtonIcon.addClass("bi-list");
+			navButtonIcon.removeClass("text-dark");
+
+			// Close nav
+			nav.removeClass("open");
+		}
+	};
+
+	navButton.click(handleNav);
+	body.click(closeNav);
+	nav.click((event) => {
+		// Stop it from closing the nav when nav is clicked
+		event.stopPropagation();
 	});
 
-	// // Make nav button right align with the container
-	// const containerMargin = parseInt($(".container").css("marginLeft"));
-	//
-	// // Initial call
-	// const handleFixedMargin = (margin) => {
-	// 	navButton.css("margin-right", margin);
-	// };
-	// handleFixedMargin(containerMargin);
-	// // Set it when the screen size changes
-	// $(window).resize(() => {
-	// 	const containerMargin = parseInt($(".container").css("marginLeft"));
-	// 	handleFixedMargin(containerMargin);
-	// });
+	// Make nav and skip to content to align with the container
+	// on tablet devices an up
+	const skipMain = $("#skip-main");
+	const skipRestaurant = $("#skip-restaurant");
+	const containerMargin = parseInt($(".container").css("marginLeft"));
+
+	// Initial call
+	const handleFixedMargin = (margin) => {
+		// Only apply it on tablet and up
+		const mediaTablet = matchMedia("only screen and (min-width: 768px)");
+		if (mediaTablet.matches) {
+			navButton.css("margin-right", margin);
+			nav.css("margin-right", margin);
+			skipMain.css("margin-left", margin);
+			skipRestaurant.css("margin-left", margin);
+		}
+	};
+	handleFixedMargin(containerMargin);
+	// Set it when the screen size changes
+	$(window).resize(() => {
+		const containerMargin = parseInt($(".container").css("marginLeft"));
+		handleFixedMargin(containerMargin);
+	});
 });
