5f44a67e7d1d05a359541a64ac89d26aade0b2d5
Bishram <m.bishram.yashir@gmail.com>
2021-07-16 10:55:41 +0800

    adding get restaurant detail test

--- specs/get-restaurant-detail.spec.ts
+++ specs/get-restaurant-detail.spec.ts
@@ -3,25 +3,25 @@ import { ENDPOINTS } from "@scripts/constants/endpoints";
 import { FetchData } from "@scripts/data/fetch-data";
 import { AlertHelper } from "@utils/alert-helper";
 
-describe("Get all restaurant", () => {
+describe("Get restaurant detail", () => {
+	beforeEach(() => {
+		fetchMock.mock(ENDPOINTS.DETAIL("1"), 500);
+	});
+
 	afterEach(() => {
 		fetchMock.restore();
 	});
 
 	describe("when fetch is failed", () => {
-		beforeEach(() => {
-			fetchMock.mock(ENDPOINTS.LIST, 500);
-		});
-
-		it("should return an empty array", async () => {
-			const res = await FetchData.restaurantList();
-			expect(res).toEqual([]);
+		it("should return an empty object", async () => {
+			const res = await FetchData.restaurantDetail("1");
+			expect(res).toEqual({});
 		});
 
 		it("should show notification", async () => {
 			spyOn(AlertHelper, "createAlert");
 
-			await FetchData.restaurantList();
+			await FetchData.restaurantDetail("1");
 			expect(AlertHelper.createAlert).toHaveBeenCalled();
 		});
 	});
